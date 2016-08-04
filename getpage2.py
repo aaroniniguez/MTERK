bce7f49a6e8fe3d0c320eabc93920f34cbcfd4c8
root <root@ip-172-30-0-152.us-west-2.compute.internal>
2016-08-04 18:52:44 +0000

    edited getpage2

--- getpage2.py
+++ getpage2.py
@@ -5,8 +5,7 @@ from lxml.etree import tostring
 import requests
 #data = ["clp.com/FAQs+1"]
 def getIt(data1):
-	useragent1 = "Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
-	useragent2 = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36"
+	useragent = "Mozilla/5.0 (Linux; U; Android 2.2; en-us; Nexus One Build/FRF91) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1"
 	url = "http://"+data1
 	url2 = "https://"+data1
 	if ("www." not in data1):
@@ -17,7 +16,7 @@ def getIt(data1):
 		url3 = url2.replace("www.", "")
 	headers = {
 		"Cache-Control":"max-age=0",
-		"User-Agent":useragent2,
+		"User-Agent":useragent,
 		"Accept-Language":"en-US,en;q=0.8",
 		"Connection":"keep-alive",
 		"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -46,7 +45,6 @@ def getIt(data1):
 	if r != "undefined":
 		if ("traffic from your computer network" in r.text) or ("Please enter the text below" in r.text) or ("CGI-ODSC Taiga" in r.text) or ("To continue, please type the characters below:" in r.text) or ("403 Forbidden" in r.text ) or ("page not found" in r.text) or ("don't have permission to access" in r.text) or ("503 Service" in r.text) or ("MOE Denied Access Policy" in r.text) or ("IP has been automatically blocked" in r.text):
 			r = "undefined"
-	failed = 0
 	if "undefined" not in r:
 		helper.correctCoding(r)
 		if r.encoding is None:
